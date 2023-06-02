6770326cb6d3d36fecffb1a18d0ec6ccd2b41e5d
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-06-02 18:08:54 +0600

    added submit button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser = event => {
+    event.preventDefault()
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+  }
 
   return (
     <>
-      
       <h1>Simple Crud</h1>
-      
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
