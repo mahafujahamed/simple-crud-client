bb1ba3a7543bdc0dfcdde69ae26ff24b7a4d9542
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-06-05 15:54:00 +0600

    added fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import './App.css'
 function App() {
 
   const handleAddUser = event => {
-    event.preventDefault()
-    const name = event.target.name.value;
-    const email = event.target.email.value;
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
     const user = { name, email };
     console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
   }
 
   return (
@@ -25,4 +38,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
